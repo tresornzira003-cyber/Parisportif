a0dcf1d73b81b29982012e18a311c04e5665a17b
tresornzira003-cyber <tresornzira003@gmail.com>
2026-07-16 23:47:14 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+for i in range(5): print("cette ligne se répète automatiquement  !") 
+score = 12
+
+if score >= 10:
+    print("Bravo, tu as gagné ! 🎉")
+else:
+    print("Dommage, tu as perdu. Essaye encore ! 🔄")
